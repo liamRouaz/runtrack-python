cdeb158b0207626109d56aa36669c222b180cdd2
liam-rouaz <145978285+liamRouaz@users.noreply.github.com>
2023-11-22 21:48:21 +0100

    Delete Job07 directory

--- Job07/job07.py
+++
@@ -1,5 +0,0 @@
-num1= 40
-num2= 2
-somme= num1 + num2
-#J'utilise la vigule pour separer mes element de la liste car ont ne peut pas concatener des int est des str avec +
-print("la somme de num 1 et 2 est de :" , somme)
