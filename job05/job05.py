89af141105da8a6cf8a18902e92d5b997d557047
liam-rouaz <145978285+liamRouaz@users.noreply.github.com>
2023-11-22 21:50:28 +0100

    Delete job05 directory

--- job05/job05.py
+++
@@ -1,8 +0,0 @@
-# Fonction pour afficher à l'envers utiliser def pour declarer la fontion et le slicing pour pour lire dans le sens inverse.
-def afficher_alphabet_a_lenvers():
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    alphabet_envers = alphabet[::-1]
-    print(alphabet_envers)
-
-#pour appeller la fonction
-afficher_alphabet_a_lenvers()
