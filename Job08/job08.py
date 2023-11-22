e77741e3c71037dcfc276661a9572f2fdaae91ff
liam-rouaz <145978285+liamRouaz@users.noreply.github.com>
2023-11-22 21:48:03 +0100

    Delete Job08 directory

--- Job08/job08.py
+++
@@ -1,5 +0,0 @@
-num1= 3
-num2= 14
-produit = num1 + num2
-#J'utilise la vigule pour separer mes element de la liste car ont ne peut pas concatener des int est des str avec +
-print("la produit de num 1 et 2 est de :" , produit)
